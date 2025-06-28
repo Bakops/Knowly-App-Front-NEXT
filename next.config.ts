ed51569f9c6558edf1bdb43d6e7db5ad3ff7fca6
Bakops <bakoutoure4@gmail.com>
2025-06-28 23:18:35 +0200

    MAJ auth0

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["s.gravatar.com", "platform-lookaside.fbsbx.com"],
+    domains: [
+      "s.gravatar.com",
+      "platform-lookaside.fbsbx.com",
+      "lh3.googleusercontent.com",
+    ],
   },
 };
 
